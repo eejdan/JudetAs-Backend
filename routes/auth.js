cd4216ee338fac88832c035de22b754555a90194
eejdan <65033345+eejdan@users.noreply.github.com>
2022-04-09 01:44:10 +0300

    added session creation

--- routes/auth.js
+++ routes/auth.js
@@ -3,35 +3,84 @@ if (require.main === module) {
 }
 const express = require('express')
 const { body, validationResult } = require('express-validator');
+const crypto = require("crypto");
+
+const mongoose = require("mongoose");
+const Test = require("../schemas/test");
+const User = require("../schemas/user")
+const UserSession = require("../schemas/userSession")
 
 const redis = require('redis');
+const userSession = require('../schemas/userSession');
 const client = redis.createClient({ 
     username: process.env.BACKEND_REDIS_USERNAME, 
     password: process.env.BACKEND_REDIS_PASSWORD,
-    url: process.env.BACKEND_REDIS_URL
+    url: process.env.BACKEND_REDIS_URL,
 });
 const router = express.Router();
 
-router.post('/admin/login', 
-    body('username').isAlphanumeric().isLength({ min: 5, max: 48 }), 
-    body('password').isString().isLength({ min: 5, max: 48 }),
-    body('authorization_code').isString().isLength({ min: 64, max: 64 }),
-    (req, res) => { 
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.sendStatus(400);
+router.post('/login', 
+    body('username').not().isEmpty().isAlphanumeric().isLength({ min: 5, max: 48 }), 
+    body('password').not().isEmpty().isString().isLength({ min: 5, max: 48 }),
+    async (req, res) => {
+        {
+            let errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                console.log(req.body)
+                return res.sendStatus(400);
+            }
         }
+
+        let hashObject = crypto.createHash("sha256");
+        let user = await User.findOne({ 
+            username: req.body.username, 
+            password: hashObject.update(req.body.password).digest('hex') 
+        }, {_id:1}).exec();
+        if(!user) {
+            console.log(user);
+            return res.sendStatus(401)
+        }
+        console.log(user)
+        let newSessionString = generateString(64);
+        let newSession = await userSession.insertOne({
+            user: user._id,
+            sessionString: newSessionString
+        })
+        res.cookie('unsolved-sid', newSessionString);
+        return res.sendStatus(200);
     }    
 )
 router.post('/admin/authorize', (req, res) => {
     
 })
-
-
+console.log("pre")
+init().catch(err => console.log(err));
 async function init() {
     client.on('error', (err) => console.log('Redis Client Error', err));
     client.on('connect', () => console.log('Redis client connected'));
-    await client.connect();
+
+    mongoose.connect(
+        'mongodb+srv://'
+        +process.env.BACKEND_MONGO_USERNAME
+        +':'+process.env.BACKEND_MONGO_PASSWORD
+        +'@'+process.env.BACKEND_MONGO_URL, {
+            dbName: 'JudetAs',
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    );
 }
-init();
+
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
 module.exports = router;

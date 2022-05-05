e30c36b82a90deb4479e83ec5712c616b785443f
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-05 09:43:47 +0300

    added util

---
+++ util/generateString.js
@@ -0,0 +1,13 @@
+
+
+function generateString(length) {
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = ''; // a nu se schimba in ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+module.exports = generateString;

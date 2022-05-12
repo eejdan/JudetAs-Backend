a29b552d525b19ea8f5841e39117325073675cbb
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-12 12:43:17 +0300

    updated schema

--- schemas/user.js
+++ schemas/user.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     username: { type: String, 
         required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     pin: { type: Object, required: false },
     registrationDate: { 
@@ -13,7 +14,6 @@ const userSchema = new mongoose.Schema({
     meta: {
         firstName:{ type: String, required: true }, //+nume de mijloc inclus
         lastName: { type: String, required: true },
-        phoneNumber: { type: String, required: false },
         CNP: { type: String, 
             required: false, unique: true, default: '' },
     }

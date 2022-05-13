e84930d1deacf26d29cc815c06eb3f3aca7e1fe5
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-13 23:20:50 +0300

    updated debugging code

--- middleware/expressValidation.js
+++ middleware/expressValidation.js
@@ -1,10 +1,11 @@
-const { validationResult } = require('express-validator')
+    const { validationResult } = require('express-validator')
 
 function expressValidation(req, res, next) {
     let errors = validationResult(req);
     if(!errors.isEmpty()) {
-        console.log('validation failed:'+JSON.stringify(req.body))
-        console.log(errors); // for debugging only
+        console.log('validation failed:');
+        console.log(req.body)
+        console.log(errors); //debugging
         return res.sendStatus(400);
     }
     next();

ce28ce6b11964662274e0a7efe2456b27e118df1
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-13 23:21:01 +0300

    redisio migration

--- middleware/tokenProcessing.js
+++ middleware/tokenProcessing.js
@@ -1,11 +1,8 @@
 
 
 const mongoose = require('mongoose')
-const client = redis.createClient({ 
-    username: process.env.BACKEND_REDIS_USERNAME, 
-    password: process.env.BACKEND_REDIS_PASSWORD,
-    url: process.env.BACKEND_REDIS_URL,
-});
+
+const client = require('../redisconnection');
 
 function tokenProcessing(req, res, next) {
     if(!req.cookies['unsolved_sid']) {

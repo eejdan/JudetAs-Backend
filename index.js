2de62206ec98ff565c999f687ec6cacaefa97493
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-15 17:17:06 +0300

    upd

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
 
 //localinstance display names will have max 64 chars
 //usernames will have max 48 chars
-app.listen(process.env.BACKEND_PORT, () => {
+app.listen(8080, () => {
     console.log("Backend server started")
 })
 setTimeout(()=> {
